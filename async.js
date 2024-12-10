8c3cb7ee3ea114d94c36b68d90854583f28f89d9
aakashnnt123 <aakash5427896@gmail.com>
2024-12-10 14:28:54 +0530

    commit

---
+++ async.js
@@ -0,0 +1,36 @@
+const fs = require("fs");
+const https  = require('https');
+
+console.log("Hello world");
+
+var a = 10786943;
+var b = 28986;
+
+
+https.get("https://dummyjson.com/products/1" , (res) =>
+{
+  console.log("fetched data successfully");
+});
+
+setTimeout(()=>{
+  console.log("setTimeout function called");
+},5000);
+
+fs.readFile("./file.txt", "utf8", (err,data)=>{
+  if(err)
+  {
+    console.error("Error Found!")
+    return;
+  }
+  console.log("File data :" ,data);
+});
+
+function multiply(x,y)
+{
+  var d = x * y;
+  return d;
+}
+
+var result = multiply(a , b);
+
+console.log(result);
---
+++ sync.js
@@ -0,0 +1,16 @@
+console.log("Hello World");
+
+var a = 10786943;
+var b = 28986;
+
+function multiply(x,y)
+{
+  var c = a*b;
+  var d = x * y;
+   console.log("C is :" , c);
+  return d;
+}
+
+var result = multiply(a , b);
+
+console.log(result);
